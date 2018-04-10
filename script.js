1246cfd508c3ef27cc872f1b464714388e0369a1
Dominik-Slawkowski <dominik.slawkowski@gmail.com>
2018-04-11 00:19:38 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+$("#pen").click(function(){
+    $("#menu").toggleClass("hide");
+});
+
+$("#stars > .material-icons").click(function () {
+    $("#stars > .material-icons").innerHTML="star";
+});
