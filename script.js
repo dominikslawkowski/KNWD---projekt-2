5270d88af0f1ae21229d9c770561d5b63ab2d205
Dominik-Slawkowski <dominik.slawkowski@gmail.com>
2018-04-11 12:21:08 +0200

    Pierwsza aktualizacja

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ $("#pen").click(function(){
     $("#menu").toggleClass("hide");
 });
 
+var oddClick = true;
+$("#pen").click(function () {
+     $("#pen > i.material-icons").text(oddClick ? "arrow_forward" : "menu");
+    oddClick = !oddClick;
+});
+
 $("#stars > .material-icons").click(function () {
-    $("#stars > .material-icons").innerHTML="star";
+    $("#stars > .material-icons").innerHTML = "star";
 });
